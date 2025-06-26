25580720b5973e841b6620e92f0854db4a67b22d
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-06-26 19:15:31 +0400

    add fix

--- backend/traders/tasks.py
+++ backend/traders/tasks.py
@@ -6,7 +6,7 @@ from exchanges.models import CandleSource, ExchangeClient
 from traders.models import Trader
 
 
-@shared_task
+@shared_task(queue='trade_loop')
 def trade_loop(timeframe: str):
 
     tf = Timeframe(timeframe)
