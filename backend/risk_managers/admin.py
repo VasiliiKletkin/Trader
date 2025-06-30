c9a4d35d6a11d9b442e58163e7b5ec9c2307621f
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-07-01 00:45:32 +0400

    add fix

--- backend/risk_managers/admin.py
+++ backend/risk_managers/admin.py
@@ -4,4 +4,11 @@ from risk_managers import models
 
 @admin.register(models.RiskManager)
 class RiskManagerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "class_name", "get_description")
+    readonly_fields = ("get_description",)
+
+    @admin.display(
+        description="Описание",
+    )
+    def get_description(self, obj: models.RiskManager) -> str:
+        return obj.get_description()
