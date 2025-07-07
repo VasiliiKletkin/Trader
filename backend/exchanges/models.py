b741d10e5a461a2289501e4d8634f3397517c16d
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-07-07 20:39:28 +0400

    add fix

--- backend/exchanges/models.py
+++ backend/exchanges/models.py
@@ -489,5 +489,5 @@ class CandleSource(ActiveManagerMixin, TimeStampedMixin, models.Model):
             candles,
             update_conflicts=True,
             update_fields=["open", "high", "low", "close", "volume"],
-            unique_fields=["candle_source", "timestamp"],
+            unique_fields=["exchange", "timeframe", "trading_pair" "timestamp"],
         )
