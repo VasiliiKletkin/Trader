7a39837e2ecdac6395f568e1637ef2c45dfd9d36
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-07-23 21:49:48 +0400

    add fix

--- backend/traders/models.py
+++ backend/traders/models.py
@@ -331,7 +331,10 @@ class Trader(TimeStampedMixin, models.Model):
                 ],
             )
             TraderOrder.objects.bulk_create(
-                [TraderOrder(trader=self, order=order) for order in orders]
+                [TraderOrder(trader=self, order=order) for order in orders],
+                ignore_conflicts=True,
+                update_fields=["order"],
+                unique_fields=["trader", "order"],
             )
 
     def sync_signals(self, trader: DomainTrader) -> None:
