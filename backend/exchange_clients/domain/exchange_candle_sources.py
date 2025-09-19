fb05ab696267ee5214a7fed7f2ede44ce60d5fe7
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-09-19 22:51:56 +0400

    add fix

--- backend/exchange_clients/domain/exchange_candle_sources.py
+++ backend/exchange_clients/domain/exchange_candle_sources.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 from exchange_clients.domain import AbstractExchangeClient
 from exchanges.domain.schemas import Candle, Timeframe, TradingPair
 
@@ -24,8 +24,8 @@ class ExchangeClientCandleSource:
 
     async def get_candles(
         self,
-        since: datetime,
-        limit: int = 100,
+        since: Optional[datetime] = None,
+        limit: Optional[int] = None,
     ) -> List[Candle]:
         return await self.exchange_client.get_candles(
             trading_pair=self.trading_pair.symbol,
