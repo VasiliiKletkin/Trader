4f1fee10e229b97704cfb844dc7900068c1f3db9
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-09-20 15:49:03 +0400

    add fix

--- backend/traders/tasks.py
+++ backend/traders/tasks.py
@@ -49,7 +49,7 @@ def handle_candle_by_sources(sources_ids: List[int]):
 def handle_candle_for_exchange_client(
     exchange_client_id: int,
     traders_ids: List[int],
-):
+) -> None:
     """Обработка свечи для трейдеров конкретного exchange_client."""
     exchange_client: ExchangeClient = ExchangeClient.active_objects.select_related(
         "exchange"
@@ -137,79 +137,6 @@ async def trader_handle_candle_async(
         await trader.handle_candle(candle=candle, create_order=create_order)
     except Exception as e:
         logger.error(f"Ошибка в handle_candle для трейдера {trader}: {e}")
-
-
-# @shared_task()
-# def traders_check_opened_positions(timeframe: str):
-#     """Контроль открытых позиций для всех активных трейдеров."""
-#     tf = Timeframe(timeframe)
-#     trader_ids = list(
-#         Trader.objects.filter(timeframe=tf, status=TraderStatus.ENABLED).values_list(
-#             "pk", flat=True
-#         )
-#     )
-#     task_params = [{"trader_id": trader_id} for trader_id in trader_ids]
-#     run_tasks_in_groups(trader_check_opened_positions, task_params, chunk_size=20)
-
-
-# @shared_task()
-# def trader_check_opened_positions(trader_id: int):
-#     try:
-#         trader = Trader.objects.select_related(
-#             "exchange_client",
-#             "trading_pair",
-#         ).get(id=trader_id)
-#         candle = trader.candles.order_by("-timestamp").first()
-#         if candle is None:
-#             logger.warning(f"Не удалось получить свечу для трейдера {trader.pk}")
-#             return
-#         trader.check_opened_positions(candle=candle)
-#     except Trader.DoesNotExist:
-#         logger.error(f"Trader с id {trader_id} не существует.")
-#     except Exception as e:
-#         logger.error(
-#             f"Ошибка при проверке открытых позиций для трейдера {trader_id}: {e}"
-#         )
-
-
-# @shared_task()
-# def traders_handle_candle(timeframe: str):
-#     """
-#     Функция для запуска торгового цикла для всех трейдеров
-#     на заданном таймфрейме.
-#     """
-#     tf = Timeframe(timeframe)
-#     trader_ids = list(
-#         Trader.objects.filter(timeframe=tf, status=TraderStatus.ENABLED).values_list(
-#             "pk", flat=True
-#         )
-#     )
-#     task_params = [{"trader_id": trader_id} for trader_id in trader_ids]
-#     run_tasks_in_groups(trader_handle_candle, task_params, chunk_size=20)
-
-
-# @shared_task()
-# def trader_handle_candle(trader_id: int):
-#     try:
-#         trader = Trader.objects.select_related(
-#             "exchange_client",
-#             "trading_pair",
-#             "strategy",
-#             "risk_manager",
-#         ).get(id=trader_id)
-#         now = timezone.now()
-#         tf_timedelta = Timeframe(trader.timeframe).timedelta()
-#         candle = trader.get_candle_at_time(now - tf_timedelta)
-#         if candle is None:
-#             logger.warning(f"Не удалось получить свечу для трейдера {trader.pk}")
-#             return
-#         trader.handle_candle(candle=candle)
-#     except Trader.DoesNotExist:
-#         logger.error(f"Trader с id {trader_id} не существует.")
-#     except Exception as e:
-#         logger.error(
-#             f"Ошибка при проверке открытых позиций для трейдера {trader_id}: {e}"
-#         )
 
 
 @shared_task(queue="trader_reboot")
