3d495f19dc6222e75594a7e8e8262fde6c7dc9d5
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-09-26 23:16:56 +0400

    add fix

--- backend/exchange_clients/domain/proxies.py
+++ backend/exchange_clients/domain/proxies.py
@@ -1,3 +1,15 @@
-
 class Proxy:
-    pass
+
+    def __init__(
+        self,
+        protocol: str,
+        host: str,
+        port: str,
+        username: str,
+        password: str,
+    ):
+        self.protocol = protocol
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
