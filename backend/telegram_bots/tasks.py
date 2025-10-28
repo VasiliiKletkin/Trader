94ed36cf33f00a5dc69c0f5c23ab2aacfcc12468
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-10-28 15:13:59 +0400

    add fix

--- backend/telegram_bots/tasks.py
+++ backend/telegram_bots/tasks.py
@@ -1,4 +1,5 @@
 import asyncio
+
 from aiogram import Bot
 from aiogram.exceptions import (
     TelegramBadRequest,
@@ -6,8 +7,25 @@ from aiogram.exceptions import (
     TelegramRetryAfter,
 )
 from celery import shared_task
-
+from django.db import models
 from telegram_bots.models import TelegramBot, TelegramChat
+
+
+async def async_send_notification(token: str, chat_ids: list[str], message: str) -> None:
+    bot = Bot(token=token)
+    for chat_id in chat_ids:
+        try:
+            await bot.send_message(chat_id=chat_id, text=message)
+        except TelegramRetryAfter as e:
+            print(f"Flood control: Повтор через {e.retry_after} сек для чата {chat_id}")
+            await asyncio.sleep(e.retry_after)
+        except TelegramBadRequest as e:
+            print(f"Ошибка при отправке сообщения в чат {chat_id}: {e}")
+        except TelegramNetworkError as e:
+            print(f"Сетевая ошибка при отправке сообщения в чат {chat_id}: {e}")
+        except Exception as e:
+            print(f"Неизвестная ошибка при отправке в чат {chat_id}: {e}")
+    await bot.session.close()
 
 
 @shared_task(queue="send_notifications")
@@ -21,35 +39,15 @@ def send_notification(message: str) -> None:
         return
 
     token = active_bot.token
-    chats = TelegramChat.active_objects.filter(bot=active_bot)
+    chats: models.QuerySet[TelegramChat] = TelegramChat.active_objects.filter(
+        bot=active_bot
+    )
     if not chats.exists():
         print("Нет чатов для отправки уведомления.")
         return
 
-    async def send_notification(token: str, chat_ids: list[str], message: str) -> None:
-        bot = Bot(token=token)
-        try:
-            for chat_id in chat_ids:
-                try:
-                    await bot.send_message(chat_id=chat_id, text=message)
-                    await asyncio.sleep(0.1)
-                except TelegramRetryAfter as e:
-                    print(
-                        f"Flood control: Повтор через {e.retry_after} сек для чата {chat_id}"
-                    )
-                    await asyncio.sleep(e.retry_after)
-                    await bot.send_message(chat_id=chat_id, text=message)
-                except TelegramBadRequest as e:
-                    print(f"Ошибка при отправке сообщения в чат {chat_id}: {e}")
-                except TelegramNetworkError as e:
-                    print(f"Сетевая ошибка при отправке сообщения в чат {chat_id}: {e}")
-                except Exception as e:
-                    print(f"Неизвестная ошибка при отправке в чат {chat_id}: {e}")
-        finally:
-            await bot.close()
-
     asyncio.run(
-        send_notification(
+        async_send_notification(
             token=token, chat_ids=[chat.chat_id for chat in chats], message=message
         )
     )
