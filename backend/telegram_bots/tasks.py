ef7c7fde7f3c0b5feba9f45cfc6b83bdd81fae2a
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-10-28 14:56:45 +0400

    add fix

--- backend/telegram_bots/tasks.py
+++ backend/telegram_bots/tasks.py
@@ -28,21 +28,25 @@ def send_notification(message: str) -> None:
 
     async def send_notification(token: str, chat_ids: list[str], message: str) -> None:
         bot = Bot(token=token)
-        for chat_id in chat_ids:
-            try:
-                await bot.send_message(chat_id=chat_id, text=message)
-            except TelegramRetryAfter as e:
-                print(
-                    f"Flood control: Повтор через {e.retry_after} сек для чата {chat_id}"
-                )
-                await asyncio.sleep(e.retry_after)
-                await bot.send_message(chat_id=chat_id, text=message)
-            except TelegramBadRequest as e:
-                print(f"Ошибка при отправке сообщения в чат {chat_id}: {e}")
-            except TelegramNetworkError as e:
-                print(f"Сетевая ошибка при отправке сообщения в чат {chat_id}: {e}")
-            except Exception as e:
-                print(f"Неизвестная ошибка при отправке в чат {chat_id}: {e}")
+        try:
+            for chat_id in chat_ids:
+                try:
+                    await bot.send_message(chat_id=chat_id, text=message)
+                    await asyncio.sleep(0.1)
+                except TelegramRetryAfter as e:
+                    print(
+                        f"Flood control: Повтор через {e.retry_after} сек для чата {chat_id}"
+                    )
+                    await asyncio.sleep(e.retry_after)
+                    await bot.send_message(chat_id=chat_id, text=message)
+                except TelegramBadRequest as e:
+                    print(f"Ошибка при отправке сообщения в чат {chat_id}: {e}")
+                except TelegramNetworkError as e:
+                    print(f"Сетевая ошибка при отправке сообщения в чат {chat_id}: {e}")
+                except Exception as e:
+                    print(f"Неизвестная ошибка при отправке в чат {chat_id}: {e}")
+        finally:
+            await bot.close()
 
     asyncio.run(
         send_notification(
