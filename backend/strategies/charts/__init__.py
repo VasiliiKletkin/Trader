093cec370ab449fafb952a0b7b88882d28160cbf
SafiullinFanil <safiullin.fanil@bk.ru>
2025-11-12 00:06:52 +0300

    Добавил графики + схема

--- backend/strategies/charts/__init__.py
+++ backend/strategies/charts/__init__.py
@@ -1,10 +1,12 @@
 from .renko import app as RenkoStrategyChart
 from .mfi import app as MoneyFlowIndexStrategy
 from .stochastic import app as StochasticStrategyChart
+from .donchian_crossover import app as DonchianCrossoverStrategy
 
 
 __all__ = [
     "RenkoStrategyChart",
     "MoneyFlowIndexStrategy",
     "StochasticStrategyChart",
+    "DonchianCrossoverStrategy",
 ]
