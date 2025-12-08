9a4180218efd9c8a4387f8248bc825a6364dd45a
Vasilii Kletkin <kletkinvasilii@gmail.com>
2025-12-08 19:08:20 +0300

    add fix

--- backend/traders/admin.py
+++ backend/traders/admin.py
@@ -220,7 +220,7 @@ class TraderAdmin(admin.ModelAdmin):
     @admin.action(description="Перезагрузить трейдеры")
     def reboot_trader(self, request, queryset: models.QuerySet[Trader]):
         for trader in queryset:
-            trader_reboot(trader_id=trader.pk)
+            trader_reboot.delay(trader_id=trader.pk)
         self.message_user(
             request,
             f"{queryset.count()} трейдер(ов) перезагружается.",
