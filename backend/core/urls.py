1a490d8ca2f00c07572795399aea459910751163
Vasilii Kletkin <kletkinvasilii@gmail.com>
2026-02-14 13:22:15 +0300

    add fix

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
     path("traders/", include("traders.urls")),
-    path("traders/", include("arbitrage_traders.urls")),
+    path("arbitrage-traders/", include("arbitrage_traders.urls")),
     path("candle-sources/", include("candle_sources.urls")),
     # Health check endpoints
     path("health/", health_check, name="health"),
