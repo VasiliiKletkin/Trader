5bd68af8af15ba603d3d09b70505343b3382bb47
Vasilii Kletkin <kletkinvasilii@gmail.com>
2026-04-23 02:30:35 +0300

    add fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -92,7 +92,7 @@ if DEBUG:
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
